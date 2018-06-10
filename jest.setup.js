e2fa4613e7817ab6c949e8d95b94d0d3f0c79099
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2018-06-10 22:06:11 +0000

    Implement move command

--- jest.setup.js
+++ jest.setup.js
@@ -1,22 +1,32 @@
-const should = require('should');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 const config = require('config');
+const Promise = require('bluebird');
 const clientSocket = require("socket.io-client");
 const Robot = require('./lib/model/Robot');
+
+chai.use(chaiAsPromised);
+chai.should();
 
 jest.setTimeout(3000);
 
 beforeAll(async () => {
   global.server = require('./lib/server');
-  const linda = new Robot({name: 'Linda', key: 'LindaKey', secret: 'LindaSecret', x: 0, y: 0, parts: [{type: 'engine', range: 5, drain: 5}]});
-  return linda.save();
 });
 
 afterAll((done) => {
   global.server.close(done);
 });
 
-beforeEach((done) => {
+beforeEach(async (done) => {
+  await Robot.removeAsync({});
+  const linda = new Robot({name: 'Linda', key: 'LindaKey', secret: 'LindaSecret', x: 0, y: 0, parts: [
+    {type: 'engine', range: 5, drain: 5},
+    {type: 'battery', charge: 40, capacity: 50}
+  ]});
+  await linda.save();
   global.client = clientSocket(`http://localhost:${config.get('port')}`);
+  Promise.promisifyAll(global.client);
   global.client.emit('authentication', {key: 'LindaKey', secret: 'LindaSecret'});
   global.client.on('authenticated', () => done());
 });
--- test/move.test.js
+++ test/move.test.js
@@ -1,23 +1,22 @@
-test('move', (done) => {
-  client.emit('move', {}, function(result) {
-    result.error.should.equal('wrong-arguments');
-    done();
-  });
-  client.emit('move', {x: 4.5, y: 4.5}, function(result) {
-    result.error.should.equal('wrong-arguments');
-    done();
-  });
-  client.emit('move', {x: 0, y: 0}, function(result) {
-    result.error.should.equal('already-there');
-    done();
-  });
-  client.emit('move', {x: 10, y: 10}, function(result) {
-    result.error.should.equal('insufficient-engine-range');
-    done();
-  });
-  client.emit('move', {x: 1, y: 1}, function(result) {
-    result.x.should.equal(1);
-    result.y.should.equal(1);
-    done();
-  });
+const Robot = require('../lib/model/Robot');
+
+test('move', async () => {
+  const result = await client.emitAsync('move', {x: 1, y: 1});
+  result.x.should.equal(1);
+  result.y.should.equal(1);
+  result.battery.charge.should.equal(35);
+  result.battery.capacity.should.equal(50);
+  await client.emitAsync('move', {}).should.eventually.be.rejected.with.property('error').that.equals('wrong-arguments');
+  await client.emitAsync('move', {x: 4.5, y: 4.5}).should.eventually.be.rejected.with.property('error').that.equals('wrong-arguments');
+  await client.emitAsync('move', {x: 1, y: 1}).should.eventually.be.rejected.with.property('error').that.equals('already-there');
+  await client.emitAsync('move', {x: 10, y: 10}).should.eventually.be.rejected.with.property('error').that.equals('insufficient-engine-range');
+  await Robot.updateAsync({key: 'LindaKey', 'parts.type': 'battery'}, {$set: {'parts.$.charge': 0}});
+  await client.emitAsync('refresh');
+  await client.emitAsync('move', {x: 2, y: 2}).should.eventually.be.rejected.with.property('error').that.equals('insufficient-charge');
+  await Robot.updateAsync({key: 'LindaKey'}, {$pull: {parts: {type: 'battery'}}});
+  await client.emitAsync('refresh');
+  await client.emitAsync('move', {x: 2, y: 2}).should.eventually.be.rejected.with.property('error').that.equals('no-battery');
+  await Robot.updateAsync({key: 'LindaKey'}, {$pull: {parts: {type: 'engine'}}});
+  await client.emitAsync('refresh');
+  await client.emitAsync('move', {x: 2, y: 2}).should.eventually.be.rejected.with.property('error').that.equals('no-engine');
 });
