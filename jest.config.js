ab645976cf317d90f757db582762be0c0e85beb4
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2018-06-10 13:04:29 +0000

    Fix tests

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const {defaults} = require('jest-config');
+
+process.env['NODE_ENV'] = 'test';
+
+module.exports = {};
--- test/info.test.js
+++ test/info.test.js
@@ -1,5 +1,34 @@
+const _ = require('lodash');
 const should = require('should');
+const config = require('config');
+const serverSocket = require("socket.io");
+const clientSocket = require("socket.io-client");
+let server, client;
 
-test('adds 1 + 2 to equal 3', () => {
-  (1 + 2).should.equal(3);
+beforeAll(() => {
+  server = serverSocket(config.get('port'));
+  server.on('connection', (socket) => {
+    socket.on('info', (cb) => {
+      cb(_.pick(require('../package.json'), ['name', 'repository', 'version']));
+    });
+  });
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+beforeEach(() => {
+  client = clientSocket(`http://localhost:${config.get('port')}`);
+});
+
+afterEach(() => {
+  client.disconnect();
+});
+
+test('info', (done) => {
+  client.emit('info', (info) => {
+    info.name.should.equal('spacemine');
+    done();
+  });
 });
